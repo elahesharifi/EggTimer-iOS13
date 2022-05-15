2f72ff31210f10b372d48667f257bb1cd4fc05db
elahe <el.sh192014@gmail.com>
2022-05-16 01:16:00 +0200

    egg timer completed project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var progreeBarView: UIProgressView!
     
-    let eggTimes = ["Soft":3, "Medium":4, "Hard":7]
+    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
