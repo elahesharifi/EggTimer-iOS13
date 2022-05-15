be23ba63c6c344f1867e41aed21dd85dd2c01eb7
elahe <el.sh192014@gmail.com>
2022-05-16 01:05:03 +0200

    add counter to progressBarView
    change progress with the value of eggTimes

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,24 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var progreeBarView: UIProgressView!
     
     let eggTimes = ["Soft":3, "Medium":4, "Hard":7]
-    var secondRemaining = 60
     var timer = Timer()
-    
+    var totalTime = 0
+    var secondsPassed = 0
+   
     @IBAction func hardnessSelected(_ sender: UIButton) {
-       
-        progreeBarView.progress = 1.0
+
         timer.invalidate() //set timer with 0 amount that every time selected or we can say reset it
-        
         let hardness = sender.currentTitle! //Soft , Medium , Hard
-        secondRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        progreeBarView.progress = 0.0
+        secondsPassed = 0
+        statusLabel.text = hardness
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
        
     }
     @objc func updateTimer(){
-        if secondRemaining > 0 {
-            print("\(secondRemaining) seconds.")
-            secondRemaining -= 1
-        }else{
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progreeBarView.progress = Float(secondsPassed) / Float(totalTime)
+           
+        }else {
             timer.invalidate()
             statusLabel.text = "Done!"
             
